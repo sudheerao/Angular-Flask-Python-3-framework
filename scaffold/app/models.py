1348452156b8ed83ec23697aa4337ebd0d1ee415
Excellent1212 <frankgass89@gmail.com>
2015-11-27 21:34:13 +0530

    validation

--- scaffold/app/models.py
+++ scaffold/app/models.py
@@ -14,10 +14,11 @@ class {Resources}(db.Model, CRUD_MixIn):
 class {Resources}Schema(Schema):
 
     not_blank = validate.Length(min=1, error='Field cannot be blank')
+    #add validate=not_blank in required fields
     id = fields.Integer(dump_only=True)
     {schema}
     
-    #self links
+    #self links    
     def get_top_level_links(self, data, many):
         if many:
             self_link = "/{resources}/"
