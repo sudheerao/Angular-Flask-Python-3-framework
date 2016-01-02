934ded58c20b487607679b224209a14dafbe65c8
Excellent1212 <frankgass89@gmail.com>
2016-01-02 15:33:22 +0530

    index and app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,84 +3,21 @@ angular.module('myApp', ['ui.router', 'ngResource',  "angularGrid" , 'myApp.cont
 angular.module('myApp').config(function( $stateProvider , $urlRouterProvider, $authProvider) {
 
 
- $stateProvider  
+ $stateProvider
    .state('home', {
     url: '/',
-    templateUrl: 'home.html',    
-    
-  
+    templateUrl: 'home.html',
+
+
   })
-   
-   
-   
-   
-   
+
+
    // States
-  // Routes for authors
-  .state('authors', {
-    url: '/authors',
-    templateUrl: 'authors/index.html',
-    controller: 'AuthorListController',
-
-
-  }).state('newAuthor', {
-    url: '/authors/new',
-    templateUrl: '/authors/add.html',
-    controller: 'AuthorCreateController',
-
-    }).state('editAuthor', {
-    url: '/authors/:id/edit',
-    templateUrl: 'authors/update.html',
-    controller: 'AuthorEditController',
-
-        })
-
-        // End Routes for authors
-  // Routes for comments
-  .state('comments', {
-    url: '/comments',
-    templateUrl: 'comments/index.html',
-    controller: 'CommentListController',
-
-
-  }).state('newComment', {
-    url: '/comments/new',
-    templateUrl: '/comments/add.html',
-    controller: 'CommentCreateController',
-
-    }).state('editComment', {
-    url: '/comments/:id/edit',
-    templateUrl: 'comments/update.html',
-    controller: 'CommentEditController',
-
-        })
-
-        // End Routes for comments
-  // Routes for posts
-  .state('posts', {
-    url: '/posts',
-    templateUrl: 'posts/index.html',
-    controller: 'PostListController',
-
-
-  }).state('newPost', {
-    url: '/posts/new',
-    templateUrl: '/posts/add.html',
-    controller: 'PostCreateController',
-
-    }).state('editPost', {
-    url: '/posts/:id/edit',
-    templateUrl: 'posts/update.html',
-    controller: 'PostEditController',
-
-        })
-
-        // End Routes for posts
- 
- 
-   
-  ;
   
+
+
+  ;
+
   })
   .directive('stringToNumber', function() {
   return {
@@ -100,20 +37,19 @@ angular.module('myApp').config(function( $stateProvider , $urlRouterProvider, $a
     restrict: 'A',
     require: 'ngModel',
     link: function (scope, element, attrs, ngModel) {
-      
+
       //format text going to user (model to view)
       ngModel.$formatters.push(function(date) {
         return new Date(date);
       });
-      
+
       //format text from the user (view to model)
      // ngModel.$parsers.push(function(value) {
       //  return value.toLowerCase();
      // });
     }
   }
-});        
-  
+});
+
 angular.module('myApp.services', []);
 angular.module('myApp.controllers', []);
-
