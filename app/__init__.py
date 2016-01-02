2c28fe789f3fdfac3f5f20daff9d12f379dbf0ac
Excellent1212 <frankgass89@gmail.com>
2016-01-02 16:47:33 +0530

    app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 # http://flask.pocoo.org/docs/0.10/patterns/appfactories/
 
+
 def create_app(config_filename):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='templates/static')
     app.config.from_object(config_filename)
 
-    #For Dev purposes only can be removed in Production
+    # For Dev purposes only can be removed in Production
     from flask import render_template, send_from_directory
     import os
+
     @app.route('/')
     def index():
         return render_template('index.html')
@@ -17,12 +19,17 @@ def create_app(config_filename):
 
         return send_from_directory(os.path.join(app.root_path, 'templates'), filename)
 
-    #End
+    # End
 
     from app.basemodels import db
     db.init_app(app)
 
     # Blueprints
-
+    from app.comments.views import comments
+    app.register_blueprint(comments, url_prefix='/api/v1/comments')
+    from app.authors.views import authors
+    app.register_blueprint(authors, url_prefix='/api/v1/authors')
+    from app.posts.views import posts
+    app.register_blueprint(posts, url_prefix='/api/v1/posts')
 
     return app
