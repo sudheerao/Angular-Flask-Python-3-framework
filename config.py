f7fbd557a1f1ed61edb5e7b8db47432358a31bf5
root <root@seven.leog.in>
2016-01-19 10:31:42 +0000

    config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ mysql_db_hostname = 'localhost'
 
 DEBUG = True
 PORT = 5000
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False
 SECRET_KEY = "SOME SECRET"
 # PostgreSQL
@@ -26,3 +26,13 @@ SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}"
                                                                                         DB_PASS=mysql_db_password,
                                                                                         DB_ADDR=mysql_db_hostname,
                                                                                         DB_NAME=mysql_db_name)"""
+# Email Server Configuration
+
+MAIL_DEFAULT_SENDER = "leo@localhost"
+
+PASSWORD_RESET_EMAIL ="""
+    Hi,
+
+      Please click on the link below to reset your password
+
+      <a href="/forgotpassword/{token}> Click here </a>"""
