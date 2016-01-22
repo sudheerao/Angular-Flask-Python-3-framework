9f1c314c11a26e64ae76592f160789b5d19d7c75
Excellent1212 <frankgass89@gmail.com>
2016-01-22 13:19:58 +0000

    protractor conf.js

---
+++ conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: [  
+   '/home/travis/build/Leo-G/Flask-Scaffold/app/templates/users/spec.js'
+   '/home/travis/build/Leo-G/Flask-Scaffold/app/templates/devs/spec.js'
+  ]
+}
+
