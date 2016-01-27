2d1cedba1c27c9984b52bf4233c7359445c66681
Excellent1212 <frankgass89@gmail.com>
2016-01-27 15:51:35 +0000

    google analytics

--- app/templates/static/js/app.js
+++ app/templates/static/js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ui.router', 'ngResource',  "angularGrid" , 'myApp.controllers', 'myApp.services', 'satellizer','toaster', 'ngAnimate']);
+angular.module('myApp', ['ui.router', 'ngResource',  "angularGrid" , 'myApp.controllers', 'myApp.services', 'satellizer','toaster', 'ngAnimate', 'angular-google-analytics']);
 
 angular.module('myApp')
   .run( function($rootScope, $state){
@@ -8,7 +8,11 @@ angular.module('myApp')
                 }
     );
 
-angular.module('myApp').config(function( $stateProvider , $urlRouterProvider, $authProvider) {
+angular.module('myApp').config(function( $stateProvider , $urlRouterProvider, $authProvider, AnalyticsProvider) {
+
+   // Google Analytics
+    AnalyticsProvider.setAccount('UA-37519052-11');
+    AnalyticsProvider.setDomainName('seven.leog.in');    
 
    // Satellizer configuration that specifies which API
   // route the JWT should be retrieved from
@@ -215,3 +219,7 @@ $stateProvider.state('login', {
 
 angular.module('myApp.services', []);
 angular.module('myApp.controllers', []);
+
+angular.module('myApp').run(function(Analytics) {
+            Analytics.pageView();
+ });
