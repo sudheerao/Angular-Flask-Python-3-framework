157b841338fc8ebebf26359cd2bc5513cc421f48
Excellent1212 <frankgass89@gmail.com>
2018-11-04 12:55:53 +0800

    login form

--- app/templates/static/src/app/login/login.component.ts
+++ app/templates/static/src/app/login/login.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
+import { User } from '../users/user';
+import { Subscription } from 'rxjs';
+import { UsersService } from '../users/users.service';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
 
 @Component({
     selector: 'app-login',
@@ -8,12 +14,36 @@ import { routerTransition } from '../router.animations';
     styleUrls: ['./login.component.scss'],
     animations: [routerTransition()]
 })
-export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+export class LoginComponent  {
 
-    ngOnInit() {}
+    LoginSubs: Subscription;
+    obj :any;
+    LoginForm = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+      });
 
+    constructor(private usersApi: UsersService, private router: Router, ) { 
+             }
+    
+    
     onLoggedin() {
+
+        console.log(this.LoginForm.value)
+        console.log(this.LoginForm.value.email)
+
+        this.obj = {
+            "data": {
+              "type": "users",
+              "attributes": {
+                  "email" : this.LoginForm.value.email,
+                  "password" : this.LoginForm.value.password
+              }
+                
+              }
+            }
+
+
         localStorage.setItem('isLoggedin', 'true');
     }
 }
--- app/templates/static/src/app/login/login.module.ts
+++ app/templates/static/src/app/login/login.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule],
+    imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {}
