254af09b5dcd8c6a72f046bf3ef75b10db011ef1
Excellent1212 <frankgass89@gmail.com>
2018-11-14 21:06:41 +0800

    update user form n tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
 driver.get("http://localhost:5000")
 assert "Flask-Scaffold" in driver.title
 email = driver.find_element_by_id("email")
@@ -15,6 +15,8 @@ password.send_keys("54321")
 login_button = driver.find_element_by_name("login")
 actions = ActionChains(driver)
 actions.click(login_button)
+heading = driver.find_element_by_tag_name('h2')
+assert "Dashboard Statistics Overview" in heading.text
 time.sleep(5)
 
 driver.close()
