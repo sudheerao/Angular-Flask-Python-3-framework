517c9664501c4997c0bc7666e31aa2fdc817bf6c
Excellent1212 <frankgass89@gmail.com>
2018-11-17 22:49:54 +0800

    final minor changes

--- scaffold/app/templates/module/edit/module-routing.module.ts
+++ scaffold/app/templates/module/edit/module-routing.module.ts
@@ -1,10 +1,10 @@
 import {{ NgModule }} from '@angular/core';
 import {{ Routes, RouterModule }} from '@angular/router';
-import {{ {Resources}EditComponent }} from './{resources}-edit.component';
+import {{ {Resource}EditComponent }} from './{resources}-edit.component';
 
 const routes: Routes = [
     {{
-        path: '', component: {Resources}EditComponent
+        path: '', component: {Resource}EditComponent
     }}
 ];
 
