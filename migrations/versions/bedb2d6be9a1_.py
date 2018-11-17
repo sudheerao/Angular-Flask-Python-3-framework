e237be5c03adeb35868c413539814c1ffddf82d5
Excellent1212 <frankgass89@gmail.com>
2018-11-17 14:59:45 +0800

    removed users.yaml

---
+++ migrations/versions/bedb2d6be9a1_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: bedb2d6be9a1
+Revises: a13de1ec67d5
+Create Date: 2018-11-03 18:27:49.751900
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'bedb2d6be9a1'
+down_revision = 'a13de1ec67d5'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
